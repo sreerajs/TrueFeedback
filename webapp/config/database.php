29b13ceca9b9441f1cfeb0fdab0e3887ebad60a0
tittuvarghese <tittuhpd@gmail.com>
2018-05-01 12:14:25 +0530

    DB Update

--- webapp/config/database.php
+++ webapp/config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bytacoin'),
+            'database' => env('DB_DATABASE', 'surveyMonkey'),
             'username' => env('DB_USERNAME', 'bytacoin'),
             'password' => env('DB_PASSWORD', 'ByTsaCoinPeWs732910wordSim'),
             'unix_socket' => env('DB_SOCKET', ''),
