c400267a2ff2fe0408e9874034c1483287953d26
trycatchajith <ajithurulikunnam@gmail.com>
2018-05-02 17:50:53 +0530

    Api Calls Update

--- webapp/resources/views/composer/survey_composer.blade.php
+++ webapp/resources/views/composer/survey_composer.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-4" id="sm-template-two" onclick="setSurveyTempalte('template-two')">
+                <div class="col-md-4" id="sm-template-two">
                     <div class="c-state-card c-state-card--success">
                         <h4 class="c-state-card__title">Template Two</h4>
                         <p class="c-state-card__status">
@@ -45,27 +45,26 @@
                     <div id="stage1" class="build-wrap composer"></div>
                     <form class="render-wrap c-card"></form>
                     <button id="edit-form" class="c-btn c-btn--info c-btn--outline">Edit Form</button>
-                    <button id="edit-form" onclick="createSurvey()" class="c-btn c-btn--info c-btn--outline" style="margin-right:5px;float: right" data-toggle="modal" data-target="#modal1">Create Survey</button>
+                    <button id="edit-form" class="c-btn c-btn--info c-btn--outline" style="margin-right:5px;float: right" data-toggle="modal" data-target="#modal1">Create Survey</button>
                 </div>
             </div>
 
             <div class="c-modal modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="modal1">
             <div class="c-modal__dialog modal-dialog" role="document">
                     <div class="modal-content">
-                        <form>
                           <div class="c-card u-p-medium u-mh-auto" style="max-width:500px;">
+                              <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
                               <div class="c-field" style="margin-bottom:20px;">
                               <label class="c-field__label" for="input1">Survey Name</label>
-                              <input class="c-input" id="input1" type="text" placeholder="My First Survey" required>
+                              <input class="c-input" id="input-survey-name" name= "" type="text" placeholder="My First Survey" required="">
                               </div>
-                              <button class="c-btn c-btn--info" name="submit" type="submit">
+                              <button id="create-new-form" class="c-btn c-btn--info" name="submit" type="submit">
                                   Create
                               </button>
                               <button class="c-btn c-btn--info" data-dismiss="modal">
                                   Close
                               </button>
                           </div>
-                        </form>
                     </div>
                 </div>
             </div>
--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -1,107 +1,116 @@
 <?php
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/deploy','Deploy\Dev@deploy');
-Route::get('/deploy/composer','Deploy\Dev@deployComposer');
-Route::get('/deploy/migrate','Deploy\Dev@deployMigrate');
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+use Illuminate\Http\Request;
+
+Route::get('/deploy', 'Deploy\Dev@deploy');
+Route::get('/deploy/composer', 'Deploy\Dev@deployComposer');
+Route::get('/deploy/migrate', 'Deploy\Dev@deployMigrate');
 Route::get('/', function () {
-  return view('frontpage');
-});
-/**
-* Routes - Unauthenticated Users
-* @author Stanly Johnson (stanly.johnson@servntire.com)
-* @param string | route
-* @return class,view
-*/
-Route::group(['middleware' => ['guest']], function() {
-  /* Frontpage */
-  Route::get('/', function () {
     return view('frontpage');
-  });
-
-  /* Login */
-  Route::get('/login', function () {
-    return view('login');
-  })->name('login');
-
-  /* Register */
-  Route::get('/register', function () {
-    return view('register');
-  });
-
-  /*Forgot Password */
-  Route::get('/forgot_password', function() {
-    return view('forgot_password');
-  });
-
-  /* User Auth Controllers */
-  Route::post('/register','UserAuth\RegisterController@create');
-  Route::post('/login','UserAuth\LoginController@auth');
-  Route::post('/forgot_password_email', 'UserAuth\ForgotPasswordController@create');
 });
 /**
-* Routes - Verification
-* @author Stanly Johnson (stanly.johnson@servntire.com)
-* @param string | route
-* @return class,view
-*/
+ * Routes - Unauthenticated Users
+ * @author Stanly Johnson (stanly.johnson@servntire.com)
+ * @param string | route
+ * @return class,view
+ */
+Route::group(['middleware' => ['guest']], function() {
+    /* Frontpage */
+    Route::get('/', function () {
+        return view('frontpage');
+    });
+
+    /* Login */
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+
+    /* Register */
+    Route::get('/register', function () {
+        return view('register');
+    });
+
+    /* Forgot Password */
+    Route::get('/forgot_password', function() {
+        return view('forgot_password');
+    });
+
+    /* User Auth Controllers */
+    Route::post('/register', 'UserAuth\RegisterController@create');
+    Route::post('/login', 'UserAuth\LoginController@auth');
+    Route::post('/forgot_password_email', 'UserAuth\ForgotPasswordController@create');
+});
+/**
+ * Routes - Verification
+ * @author Stanly Johnson (stanly.johnson@servntire.com)
+ * @param string | route
+ * @return class,view
+ */
 Route::get('/verify/email/{email_token}', 'Verify\UserVerification@email');
 /**
-* Routes - logout
-* @author Stanly Johnson (stanly.johnson@servntire.com)
-* @param none
-* @return class,view,login
-*/
-Route::get('/logout','UserAuth\Logout@logout');
+ * Routes - logout
+ * @author Stanly Johnson (stanly.johnson@servntire.com)
+ * @param none
+ * @return class,view,login
+ */
+Route::get('/logout', 'UserAuth\Logout@logout');
 
 /**
-* Routes - Pages
-* Role - User
-* @author Stanly Johnson (stanly.johnson@servntire.com)
-* @param string | route
-* @return class,view
-*/
-Route::group(['middleware' => ['role:User','auth']], function() {
-  Route::get('/home','Pages\Dashboard@user');
-  Route::get('/organization','Pages\UserSettings@organization');
-  Route::get('/profile','Pages\UserSettings@profile');
-  Route::post('/profileImage','Functions\Upload@avatar');
-  Route::post('/userprofile','Pages\UserSettings@profileUpdate');
-  Route::post('/userpassword','Pages\UserSettings@profilePassword');
-  Route::post('/organizationprofile','Pages\UserSettings@organizationUpdate');
-  Route::get('/user_rewards','Pages\RewardsController@rewards');
-  Route::get('/wallet', 'Pages\WalletController@userWallet');
-  Route::get('/profile','Pages\UserSettings@profile');
-  Route::get('/mysurveys','Pages\mySurveys@mysurveys');
+ * Routes - Pages
+ * Role - User
+ * @author Stanly Johnson (stanly.johnson@servntire.com)
+ * @param string | route
+ * @return class,view
+ */
+Route::group(['middleware' => ['role:User', 'auth']], function() {
+    Route::get('/home', 'Pages\Dashboard@user');
+    Route::get('/organization', 'Pages\UserSettings@organization');
+    Route::get('/profile', 'Pages\UserSettings@profile');
+    Route::post('/profileImage', 'Functions\Upload@avatar');
+    Route::post('/userprofile', 'Pages\UserSettings@profileUpdate');
+    Route::post('/userpassword', 'Pages\UserSettings@profilePassword');
+    Route::post('/organizationprofile', 'Pages\UserSettings@organizationUpdate');
+    Route::get('/user_rewards', 'Pages\RewardsController@rewards');
+    Route::get('/wallet', 'Pages\WalletController@userWallet');
+    Route::get('/profile', 'Pages\UserSettings@profile');
+    Route::get('/mysurveys', 'Pages\mySurveys@mysurveys');
 });
 
 /**
-* Routes - Pages
-* Role - Business
-* @author Stanly Johnson (stanly.johnson@servntire.com)
-* @param string | route
-* @return class,view
-*/
-Route::group(['prefix' => 'business','middleware' => ['role:Business','auth']], function() {
-  Route::get('/home','Pages\Dashboard@user');
-  Route::get('/organization','Pages\UserSettings@organization');
-  Route::get('/profile','Pages\UserSettings@profile');
-  Route::get('/mycontract','Pages\UserSettings@profile');
-  Route::post('/profileImage','Functions\Upload@avatar');
-  Route::post('/userprofile','Pages\UserSettings@profileUpdate');
-  Route::post('/userpassword','Pages\UserSettings@profilePassword');
-  Route::post('/organizationprofile','Pages\UserSettings@organizationUpdate');
-  Route::get('/wallet', 'Pages\WalletController@businessWallet');
-  Route::get('/mycontract', 'Pages\myContractController@contract');
-  Route::get('/deployed_contracts', 'Pages\DeployedContractsController@contract');
-  Route::get('/profile','Pages\UserSettings@profile');
-  Route::get('/survey_composer','Pages\SurveyComposerSettings@showComposer');
+ * Routes - Pages
+ * Role - Business
+ * @author Stanly Johnson (stanly.johnson@servntire.com)
+ * @param string | route
+ * @return class,view
+ */
+Route::group(['prefix' => 'business', 'middleware' => ['role:Business', 'auth']], function() {
+    Route::get('/home', 'Pages\Dashboard@user');
+    Route::get('/organization', 'Pages\UserSettings@organization');
+    Route::get('/profile', 'Pages\UserSettings@profile');
+    Route::get('/mycontract', 'Pages\UserSettings@profile');
+    Route::post('/profileImage', 'Functions\Upload@avatar');
+    Route::post('/userprofile', 'Pages\UserSettings@profileUpdate');
+    Route::post('/userpassword', 'Pages\UserSettings@profilePassword');
+    Route::post('/organizationprofile', 'Pages\UserSettings@organizationUpdate');
+    Route::get('/wallet', 'Pages\WalletController@businessWallet');
+    Route::get('/mycontract', 'Pages\myContractController@contract');
+    Route::get('/deployed_contracts', 'Pages\DeployedContractsController@contract');
+    Route::get('/profile', 'Pages\UserSettings@profile');
+    Route::get('/survey_composer', 'Pages\SurveyComposerSettings@showComposer');
+
+    Route::post('/savesurvey', function(Request $request) {
+        $response['test'] = $request->input('survey_name');
+        return json_encode($response);
+    });
 });
