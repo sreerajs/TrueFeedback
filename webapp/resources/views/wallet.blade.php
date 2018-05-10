a4d85451c8abcfae69ea03c3b9fb1f6593d445e3
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-10 23:51:32 +0530

    txn history table added

--- webapp/resources/views/wallet.blade.php
+++ webapp/resources/views/wallet.blade.php
@@ -99,48 +99,46 @@
           
           <div class="row">
               <div class="col-xl-12">
-                  <div class="c-card">
-                <h4>Accounts Overview</h4>
-                <p class="u-mb-medium">Activity from 4 Jan 2017 to 10 Jan 2017</p>
-                
-                <div class="c-feed">
-                  <div class="c-feed__item">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">just now</p>
-                  </div>
-                  
-                  <div class="c-feed__item c-feed__item--success">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">5 mins ago</p>
-                  </div>
-                  
-                  <div class="c-feed__item">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">30 mins ago</p>
-                  </div>
-                  
-                  <div class="c-feed__item c-feed__item--fancy">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">3 hours ago</p>
-                  </div>
-                  
-                  <div class="c-feed__item c-feed__item--danger">
-                    <p>Payment failed from USA by #49832</p>
-                    <p class="u-text-xsmall">5 hours ago</p>
-                  </div>
-                  
-                  <div class="c-feed__item c-feed__item--warning">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">6 hours ago</p>
-                  </div>
-                  
-                  <div class="c-feed__item">
-                    <p>New order received from Canada by #49832</p>
-                    <p class="u-text-xsmall">6 hours ago</p>
-                  </div>
-                </div><!-- // .c-feed -->
-                
-              </div>
+                <div class="c-table-responsive@wide">
+                  <table class="c-table">
+                    <thead class="c-table__head">
+                      <tr class="c-table__row">
+                        <th class="c-table__cell c-table__cell--head">TxHash</th>
+                        <th class="c-table__cell c-table__cell--head">Block</th>
+                        <th class="c-table__cell c-table__cell--head">Age</th>
+                        <th class="c-table__cell c-table__cell--head">From</th>
+                        <th class="c-table__cell c-table__cell--head">Type</th>
+                        <th class="c-table__cell c-table__cell--head">Value</th>
+                      </tr>
+                    </thead>
+  
+                    <tbody>
+                    <tr class="c-table__row">
+                        <td class="c-table__cell">
+                          0xe3309e8ca0ff0..
+                        </td>
+                        <td class="c-table__cell">5590620</td>
+                        <th class="c-table__cell">31 secs ago</th>
+                        <td class="c-table__cell">0x91c880a72..</td>
+                        <td class="c-table__cell">Ether</td>
+                        <td class="c-table__cell">0.015</td>
+                    </tr>
+
+                    <tr class="c-table__row">
+                      <td class="c-table__cell">
+                        0xe3309e8ca0ff0..
+                      </td>
+                      <td class="c-table__cell">5590620</td>
+                      <th class="c-table__cell">52 secs ago</th>
+                      <td class="c-table__cell">0x91c880a72..</td>
+                      <td class="c-table__cell">Feedback Token</td>
+                      <td class="c-table__cell">2</td>
+                  </tr>
+  
+                      
+                    </tbody>
+                  </table>
+                </div>
             </div>
             
           </div>
