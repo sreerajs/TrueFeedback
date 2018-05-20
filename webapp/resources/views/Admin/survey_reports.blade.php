ce0fe6a6c4c5e68b5bc33483825cc80974bb9255
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-20 15:37:37 +0530

    minor-fixes

--- webapp/resources/views/Admin/survey_reports.blade.php
+++ webapp/resources/views/Admin/survey_reports.blade.php
@@ -1,74 +1,64 @@
-@extends('layouts.template')
-@section('title', 'Dashboard')
-@section('body_content')
+@extends('layouts.template') @section('title', 'Dashboard') @section('body_content')
 <div class="o-page">
-    @include('layouts.sidebar')
-    @role('User')
-    @yield('user_sidebar')
-    @endrole
-    @role('Business')
-    @yield('business_sidebar')
-    @endrole
-    @role('admin')
-    @yield('admin_sidebar')
-    @endrole
+  @include('layouts.sidebar') @role('User') @yield('user_sidebar') @endrole @role('Business') @yield('business_sidebar') @endrole @role('admin') @yield('admin_sidebar') @endrole
   <main class="o-page__content">
-    @include('layouts.header')
-    @role('User')
-    @yield('user_header')
-    @endrole
-    @role('Business')
-    @yield('business_header')
-    @endrole
-    @role('admin')
-    @yield('admin_header')
-    @endrole
+    @include('layouts.header') @role('User') @yield('user_header') @endrole @role('Business') @yield('business_header') @endrole @role('admin') @yield('admin_header') @endrole
     <div class="container">
-            <div class="row">                           
-              
-                    <div class="col-12">
-                            <div class="c-table-responsive@wide">
-                              <table class="c-table">
-                                <thead class="c-table__head">
-                                  <tr class="c-table__row">
-                                    <th class="c-table__cell c-table__cell--head">Survey Name</th>
-                                    <th class="c-table__cell c-table__cell--head">Deployed</th>
-                                    <th class="c-table__cell c-table__cell--head">Created at</th>
-                                  </tr>
-                                </thead>
-                
-                                <tbody>                
-                                 @foreach ($dataArray['data'] as $survey)
-                                 <tr class="c-table__row">
-                                      <td class="c-table__cell">
-                                        <div class="o-media">                        
-                                          <div class="o-media__body">
-                                            <h6>{{$survey->name}}</h6>
-                                          </div>
-                                        </div>
-                                      </td>
-                                      <td class="c-table__cell">
-                                       @if ($survey->is_deployed != 0)   
-                                        <p style="color:green">Deployed</p>
-                                        @else
-                                        <p style="color:red">Not Deployed</p>
-                                        @endif
-                                    </td>
-                                      <td class="c-table__cell">{{$survey->created_at}}</td>
-                                   
-                                 </tr>
-                                @endforeach
-                                </tbody>
-                              </table>
-                              <br>
-                              {{ $dataArray['data']->links() }}
-                            </div>
-                          </div>
+      <div class="row">
+
+        <div class="col-12">
+          <div class="c-table-responsive@wide">
+            <table class="c-table">
+              <thead class="c-table__head">
+                <tr class="c-table__row">
+                  <th class="c-table__cell c-table__cell--head">Survey Name</th>
+                  <th class="c-table__cell c-table__cell--head">Deployed</th>
+                  <th class="c-table__cell c-table__cell--head">Created at</th>
+                  <th class="c-table__cell c-table__cell--head">Actions</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                @foreach ($dataArray['data'] as $survey)
+                <tr class="c-table__row">
+                  <td class="c-table__cell">
+                    <div class="o-media">
+                      <div class="o-media__body">
+                        <h6>{{$survey->name}}</h6>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="c-table__cell">
+                    @if ($survey->is_deployed != 0)
+                    <p style="color:green">Deployed</p>
+                    @else
+                    <p style="color:red">Not Deployed</p>
+                    @endif
+                  </td>
+                  <td class="c-table__cell">{{$survey->created_at}}</td>
+                  <td class="c-table__cell">
+                    <div class="c-dropdown dropdown">
+                      <a href="#" class="c-btn c-btn--info has-icon dropdown-toggle" id="dropdownMenuTable1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      View Stats
+                      </a>
+
+                    </div>
+          </div>
+          </td>
+          </tr>
+          @endforeach
+          </tbody>
+          </table>
+          <br> {{ $dataArray['data']->links() }}
+        </div>
+      </div>
 
 
-            </div> <!--row-->
-        </div>  <!-- container -->
-  
+    </div>
+    <!--row-->
+</div>
+<!-- container -->
+
 </main>
 </div>
 @endsection
--- webapp/resources/views/deployed_contracts.blade.php
+++ webapp/resources/views/deployed_contracts.blade.php
@@ -1,77 +1,65 @@
-@extends('layouts.template')
-@section('title', 'Dashboard')
-@section('body_content')
+@extends('layouts.template') @section('title', 'Dashboard') @section('body_content')
 <div class="o-page">
-    @include('layouts.sidebar')
-    @role('User')
-    @yield('user_sidebar')
-    @endrole
-    @role('Business')
-    @yield('business_sidebar')
-    @endrole
+  @include('layouts.sidebar') @role('User') @yield('user_sidebar') @endrole @role('Business') @yield('business_sidebar') @endrole
   <main class="o-page__content">
-    @include('layouts.header')
-    @role('User')
-    @yield('user_header')
-    @endrole
-    @role('Business')
-    @yield('business_header')
-    @endrole
+    @include('layouts.header') @role('User') @yield('user_header') @endrole @role('Business') @yield('business_header') @endrole
     <div class="container">
 
-        <div class="row">
-            <div class="col-12">
-                <div class="c-table-responsive@wide">
-                    <table class="c-table">
-                        <thead class="c-table__head">
-                            <tr class="c-table__row">
-                                <th class="c-table__cell c-table__cell--head">Survey Name</th>
-                                <th class="c-table__cell c-table__cell--head">Date</th>
-                                
-                                <th class="c-table__cell c-table__cell--head">Actions</th>
-                            </tr>
-                        </thead>
+      <div class="row">
+        <div class="col-12">
+          <div class="c-table-responsive@wide">
+            <table class="c-table">
+              <thead class="c-table__head">
+                <tr class="c-table__row">
+                  <th class="c-table__cell c-table__cell--head">Survey Name</th>
+                  <th class="c-table__cell c-table__cell--head">Date</th>
+                  <th class="c-table__cell c-table__cell--head">Status</th>
+                  <th class="c-table__cell c-table__cell--head">Actions</th>
+                </tr>
+              </thead>
 
-                        <tbody>
-                            <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-                            @foreach ($dataArray['contractList'] as $contract)
-                            <tr id = "contract-{{$contract->id}}" class="c-table__row">
-                                <td class="c-table__cell">
-                                    <div class="o-media">
-                                        <div class="o-media__body">
-                                            <h6>{{$contract->name }}</h6>                                                    
-                                        </div>
-                                    </div>
-                                </td>
-                               
-                                <td class="c-table__cell">{{$contract->updated_at }}</td>
-                                
-                                <td class="c-table__cell">
-                                    <div class="c-dropdown dropdown">
-                                        <a href="#" class="c-btn c-btn--info has-icon dropdown-toggle" id="dropdownMenuTable1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            More <i class="feather icon-chevron-down"></i>
+              <tbody>
+                <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}"> @foreach ($dataArray['contractList'] as $contract)
+                <tr id="contract-{{$contract->id}}" class="c-table__row">
+                  <td class="c-table__cell">
+                    <div class="o-media">
+                      <div class="o-media__body">
+                        <h6>{{$contract->name }}</h6>
+                      </div>
+                    </div>
+                  </td>
+
+                  <td class="c-table__cell">{{$contract->updated_at }}</td>
+                  <td class="c-table__cell">Active</td>
+
+                  <td class="c-table__cell">
+                    <div class="c-dropdown dropdown">
+                      <a href="#" class="c-btn c-btn--info has-icon dropdown-toggle" id="dropdownMenuTable1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Options <i class="feather icon-chevron-down"></i>
                                         </a>
 
-                                        <div class="c-dropdown__menu dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuTable1">
-                                            <!--<a class="c-dropdown__item dropdown-item" onclick="deployContract({{$contract->id}})">Deploy</a>-->
-                                            <a class="c-dropdown__item dropdown-item" onclick="deleteContract({{$contract->id}})">Delete</a>
-                                            <!--<a class="c-dropdown__item dropdown-item" onclick="editContract({{$contract->id}})">Edit</a>-->
-    
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>  
-                            @endforeach                                 
-                        </tbody>
-                    </table>                        
-                </div>
-                <div class="row" style="margin-top: 15px;">
-                    <div class="col-12">
-                        {{ $dataArray['contractList']->links() }}                                 
-                    </div>   
-                </div>
+                      <div class="c-dropdown__menu dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuTable1">
+                        <!--<a class="c-dropdown__item dropdown-item" onclick="deployContract({{$contract->id}})">Deploy</a>-->
+                        <a class="c-dropdown__item dropdown-item" onclick="#">View Stats</a>
+                        <a class="c-dropdown__item dropdown-item" onclick="#">Conclude Survey</a>
+                        <!--<a class="c-dropdown__item dropdown-item" onclick="editContract({{$contract->id}})">Edit</a>-->
+
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+          <div class="row" style="margin-top: 15px;">
+            <div class="col-12">
+              {{ $dataArray['contractList']->links() }}
             </div>
+          </div>
         </div>
-    </div><!-- // .container -->
+      </div>
     </div>
+    <!-- // .container -->
+</div>
 @endsection
