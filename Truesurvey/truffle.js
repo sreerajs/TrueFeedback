7becc666bdf66915bc77ca72f0421032956a76cf
sreerajs <ssreerajmailbox@gmail.com>
2018-05-23 02:57:16 +0530

    Added Ropsten Network Configuration

--- Truesurvey/truffle.js
+++ Truesurvey/truffle.js
@@ -1,4 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "dXG7QYJRJPW16SDWx2EM";
+var mnemonic = "funny stay better marble cabbage rice nuclear season congress style mass easy";
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+	gas:4612388
+    }
+  }
 };
+
