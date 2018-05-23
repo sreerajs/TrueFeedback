555e93a9a199fc669169f3b37c91833a7f908353
Stanly Johnson <stanlyjohnson@outlook.com>
2018-05-23 13:44:19 +0530

    warning-messages-integrated

--- webapp/resources/views/Wallet/wallet_menu.blade.php
+++ webapp/resources/views/Wallet/wallet_menu.blade.php
@@ -13,7 +13,67 @@
   @if (session('failed_login')) @include('layouts.alert-messages',['alertMessage'=>session('failed_login')]) @yield('alert_info') @endif
   <!-- Failed Login Message Handling -->
   <br />
-  <div class="o-page__card">
+
+  <div class="o-page__card" id="wallet-warning">
+    <div class="c-card c-card--center">
+
+      <h2 class="u-mb-medium">Wallet Setup</h2>
+      <span style="color:red">IMPORTANT INSTRUCTIONS</span><br />
+      <span style="color:grey">Please take your time to read and understand the following instructions</span>
+      <br /><br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> We cannot recover your funds or freeze your account if you visit a phishing site or lose your private key.</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> TrueFeedback wallet is not a bank. Do not consider the wallet as a bank account. TrueFeedback is only an interface.
+          When you create an account on TrueFeedback you are generating a cryptographic set of numbers: your private key and your public key (address).</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> You are simply using our interface to interact directly with the blockchain.</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> If you send your <strong>public key (address)</strong> to someone, they can send you ETH or tokens.</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> If you send your <strong>private key</strong> to someone, they now have full control of your account.</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:black">
+          <i class="feather icon-disc"> Be diligent to keep your private key and password safe. Your private key is sometimes called your mnemonic phrase, keystore file, UTC file, JSON file, wallet file.</i>
+        </p>
+      </div>
+      <br />
+      <div class="c-field">
+        <p style="color:red">
+          <i class="feather icon-disc"> If you lose your private key or password, no one can recover it.</i>
+        </p>
+      </div>
+      <br /><br />
+      <div class="c-field">
+        <center>
+          <input type="button" class="c-btn c-btn--warning" id="toggle-btn" value="I Understand">
+        </center>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="o-page__card" id="wallet-menu" style="display:none">
     <div class="c-card c-card--center">
 
       <h2 class="u-mb-medium">Wallet Setup</h2>
@@ -30,4 +90,12 @@
     </div>
   </div>
 </div>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#toggle-btn").click(function() {
+      $("#wallet-menu").toggle();
+      $("#wallet-warning").toggle();
+    });
+  });
+</script>
 @endsection
--- webapp/resources/views/wallet.blade.php
+++ webapp/resources/views/wallet.blade.php
@@ -1,22 +1,8 @@
-@extends('layouts.template')
-@section('title', 'Rewards')
-@section('body_content')
+@extends('layouts.template') @section('title', 'Rewards') @section('body_content')
 <div class="o-page">
-  @include('layouts.sidebar')
-  @role('User')
-  @yield('user_sidebar')
-  @endrole
-  @role('Business')
-  @yield('business_sidebar')
-  @endrole
+  @include('layouts.sidebar') @role('User') @yield('user_sidebar') @endrole @role('Business') @yield('business_sidebar') @endrole
   <main class="o-page__content">
-    @include('layouts.header')
-    @role('User')
-    @yield('user_header')
-    @endrole
-    @role('Business')
-    @yield('business_header')
-    @endrole
+    @include('layouts.header') @role('User') @yield('user_header') @endrole @role('Business') @yield('business_header') @endrole
 
     <div class="container">
       <div class="row">
@@ -40,117 +26,115 @@
 
                   @if ($dataArray['user']->account_type == 'Business')
                   <td class="c-table__cell">
-                      <h3> ETH Balance : <span style="color: green">{{$dataArray['user']->eth_token_balance}}</span> </h3>
-                    </td>
-                  @else
-                  @endif
+                    <h3> ETH Balance : <span style="color: green">{{$dataArray['user']->eth_token_balance}}</span> </h3>
+                  </td>
+                  @else @endif
 
-                  </tr>
+                </tr>
 
 
-                  <tr class="c-table__row">
+                <tr class="c-table__row">
 
-                    <td class="c-table__cell">
-                      <div class="c-dropdown dropdown">
-                        @if ($dataArray['user']->account_type == 'Business')
-                        <a href="/business/buytokens" class="c-btn c-btn--info" >
+                  <td class="c-table__cell">
+                    <div class="c-dropdown dropdown">
+                      @if ($dataArray['user']->account_type == 'Business')
+                      <a href="/business/buytokens" class="c-btn c-btn--info">
                           Buy Feedback Tokens
-                        </a>
-                        @else
-                        <a href="#" class="c-btn c-btn--info" >
+                        </a> @else
+                      <a href="#" class="c-btn c-btn--info">
                           Redeem Feedback Tokens
-                        </a>
-                        @endif
-                      </td>
+                        </a> @endif
+                  </td>
 
-                      <td class="c-table__cell">
-                        <div class="c-dropdown dropdown">
-                            <input type="button" class="c-btn c-btn--info" id="btn" value ="View Wallet Information">
-                        </div>
-                      </td>
-                    </tr>
-                    
+                  <td class="c-table__cell">
+                    <div class="c-dropdown dropdown">
+                      <input type="button" class="c-btn c-btn--info" id="btn" value="View Wallet Information">
+                    </div>
+                  </td>
+                </tr>
 
-                  </tbody>
-                </table>
-              </div>
+
+              </tbody>
+            </table>
             </div>
-
           </div>
 
-          <br />
+        </div>
 
-          <div class = "row" id="Create" style="display:none">
-              <div class="col-xl-12">
-                  <div class="c-card">
-            <h2 style="color: blue"> Wallet Information </h2>
-            <br />
-            <h4> Wallet Address : <span style="font-weight: bold">{{$dataArray['user']->wallet_address}}</span> </h4>
-            <br />
-            <a href="/wallet_menu" class="c-btn c-btn--info" >
+        <br />
+
+        <div class="row" id="Create" style="display:none">
+          <div class="col-xl-12">
+            <div class="c-card">
+              <h2 style="color: blue"> Wallet Information </h2>
+              <br />
+              <h4> Wallet Address : <span style="font-weight: bold">{{$dataArray['user']->wallet_address}}</span> </h4>
+              <br />
+              <a href="/wallet_menu" class="c-btn c-btn--info">
                 Modify Wallet Info
               </a>
             </div>
-            </div>
           </div>
+        </div>
 
 
-          <br />
+        <br />
 
-          <div class="row">
-              <div class="col-xl-12">
-                <div class="c-table-responsive@wide">
-                  <table class="c-table">
-                    <thead class="c-table__head">
-                      <tr class="c-table__row">
-                        <th class="c-table__cell c-table__cell--head">TxHash</th>
-                        <th class="c-table__cell c-table__cell--head">Block</th>
-                        <th class="c-table__cell c-table__cell--head">Age</th>
-                        <th class="c-table__cell c-table__cell--head">From</th>
-                        <th class="c-table__cell c-table__cell--head">Type</th>
-                        <th class="c-table__cell c-table__cell--head">Value</th>
-                      </tr>
-                    </thead>
+        <div class="row">
+          <div class="col-xl-12">
+            <div class="c-table-responsive@wide">
+              <table class="c-table">
+                <thead class="c-table__head">
+                  <tr class="c-table__row">
+                    <th class="c-table__cell c-table__cell--head">TxHash</th>
+                    <th class="c-table__cell c-table__cell--head">Block</th>
+                    <th class="c-table__cell c-table__cell--head">Age</th>
+                    <th class="c-table__cell c-table__cell--head">From</th>
+                    <th class="c-table__cell c-table__cell--head">Type</th>
+                    <th class="c-table__cell c-table__cell--head">Value</th>
+                  </tr>
+                </thead>
 
-                    <tbody>
-                    <tr class="c-table__row">
-                        <td class="c-table__cell">
-                          0xe3309e8ca0ff0..
-                        </td>
-                        <td class="c-table__cell">5590620</td>
-                        <th class="c-table__cell">31 secs ago</th>
-                        <td class="c-table__cell">0x91c880a72..</td>
-                        <td class="c-table__cell">Ether</td>
-                        <td class="c-table__cell">0.015</td>
-                    </tr>
+                <tbody>
+                  <tr class="c-table__row">
+                    <td class="c-table__cell">
+                      0xe3309e8ca0ff0..
+                    </td>
+                    <td class="c-table__cell">5590620</td>
+                    <th class="c-table__cell">31 secs ago</th>
+                    <td class="c-table__cell">0x91c880a72..</td>
+                    <td class="c-table__cell">Ether</td>
+                    <td class="c-table__cell">0.015</td>
+                  </tr>
 
-                    <tr class="c-table__row">
-                      <td class="c-table__cell">
-                        0xe3309e8ca0ff0..
-                      </td>
-                      <td class="c-table__cell">5590620</td>
-                      <th class="c-table__cell">52 secs ago</th>
-                      <td class="c-table__cell">0x91c880a72..</td>
-                      <td class="c-table__cell">Feedback Token</td>
-                      <td class="c-table__cell">2</td>
+                  <tr class="c-table__row">
+                    <td class="c-table__cell">
+                      0xe3309e8ca0ff0..
+                    </td>
+                    <td class="c-table__cell">5590620</td>
+                    <th class="c-table__cell">52 secs ago</th>
+                    <td class="c-table__cell">0x91c880a72..</td>
+                    <td class="c-table__cell">Feedback Token</td>
+                    <td class="c-table__cell">2</td>
                   </tr>
 
 
-                    </tbody>
-                  </table>
-                </div>
+                </tbody>
+              </table>
             </div>
-
           </div>
+
         </div>
-      </div><!-- // .container -->
+      </div>
+    </div>
+    <!-- // .container -->
 
-      <script type="text/javascript">
-        $(document).ready(function () {
-            $("#btn").click(function () {
-                $("#Create").toggle();
-            });
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $("#btn").click(function() {
+          $("#Create").toggle();
         });
-        </script>
+      });
+    </script>
 
-      @endsection
+    @endsection
